23b80b87eba84cc4da535d9b37bde291bad78802
3208711 <3208711@qq.com>
2016-04-22 10:50:13 +0800

    new one

---
+++ src/demo/Test.java
@@ -0,0 +1,12 @@
+package demo;
+
+public class Test {
+	public static void main(String[] args) {
+		int i = 12345678;
+//		float f = 123456789;
+		float f = i;
+		int f1 = (int)f;
+		System.out.println(f);
+		System.out.println(f1);
+	}
+}
